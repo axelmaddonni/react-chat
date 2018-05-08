f4bdd3a6c3922ea8aab3de2de7fc1d000c3f095d
Axel <axel.maddonni@gmail.com>
2018-05-08 17:24:34 -0300

    adds redux dependencies to package-json

--- chat-client/src/index.js
+++ chat-client/src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './redux/reducers'
+import handleNewMessage from './redux/sagas'
+import setupSocket from './redux/sockets'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const username = "Axel"
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, { socket, username })
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
