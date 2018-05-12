cbad478a788ee6deef487e30dfa767f64bd43802
lrabinowicz <lrabinowicz@gmail.com>
2018-05-12 13:48:41 -0300

    reducers

--- chat-client/src/redux/reducers/userList.js
+++ chat-client/src/redux/reducers/userList.js
@@ -1,15 +1,17 @@
+import { userConstants, loginConstants} from "../../constants/ActionTypes";
+
 const { Map } = require('immutable');
 const initialState = new Map();
 const userList = (state = initialState, action) => {
     console.log(action);
     switch (action.type) {
-        case 'ADD_USER':
+        case userConstants.ADD_USER:
             return state.set(action.nick, {age: action.age, city: action.city});
-        case 'DELETE_USER':
+        case userConstants.DELETE_USER:
             return state.delete(action.nick);
-        case 'POPULATE_USER_LIST':
+        case userConstants.POPULATE_USER_LIST:
             return action.list;
-        case 'LOGOUT':
+        case loginConstants.LOGOUT:
             return initialState;
         default:
             return state
