a9c77eced0cebfbb2659a9a68e507f6b33631de3
axelmaddonni <axel.maddonni@gmail.com>
2018-05-13 03:59:37 -0300

    adds tab icons

--- chat-client/src/presentational/GoToLink.js
+++ chat-client/src/presentational/GoToLink.js
@@ -4,17 +4,26 @@ import PropTypes from 'prop-types'
 
 class GoToLink extends React.Component {
     render() {
-        console.log(this.context);
-        console.log(this.context.router);
         let selected = this.context.router.route.location.pathname;
         return (
             <Link to={this.props.link}>
                 <button id={this.props.link} className={ selected === ("/" + this.props.link) ? "selected" : "" }>
-                    <i className="fa fa-user-plus fa-fw" aria-hidden="true"/>
+                    <i className={"fa fa-" + getIcon(this.props.link) } aria-hidden="true"/>
                     <span> { this.props.link.toUpperCase() } </span>
                 </button>
             </Link>
         )
+    }
+}
+
+function getIcon(link) {
+    switch (link) {
+        case "agenda":
+            return "users"
+        case "chats":
+            return "comments"
+        case "newGroup":
+            return "user-plus"
     }
 }
 
